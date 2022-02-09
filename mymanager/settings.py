339da0cdeca0278a7807efee360314ee776ceee3
Nay Htoo Aung <72061499+shamzam@users.noreply.github.com>
2022-02-09 22:40:05 +0900

    Update settings.py

--- mymanager/settings.py
+++ mymanager/settings.py
@@ -36,12 +36,7 @@ ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'pull.apps.PullConfig',
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
+    'login.apps.LoginConfig',
 ]
 
 MIDDLEWARE = [
