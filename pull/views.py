5c3e5c6bc7030fdf49d95946e65cd116e7766d81
Nay Htoo Aung <72061499+shamzam@users.noreply.github.com>
2022-02-09 19:55:31 +0900

    Update views.py

--- pull/views.py
+++ pull/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+import subprocess
 
-# Create your views here.
+def index(request):
+    #try:
+    try:
+        result = subprocess.run(["cd /var/www/terminal/terminal/ && git reset --hard && git pull"], shell=True, capture_output=True)
+        text = result.stdout.decode('utf8') + "\n"
+        if hasattr(result, 'stderr'):
+            text += text + 'stderr: ' + result.stderr.decode('utf8') + "\n"
+    except:
+        text = "aa"
+    #except subprocess.CalledProcessError as e:
+    #    result = "aa"
+    #    if hasattr(e, 'output'):
+    #        result += result + 'output: ' + e.output.decode('utf8') + "\n"
+    #    if hasattr(e, 'stderr'):
+    #        result += result + 'stderr: ' + e.stderr.decode('utf8') + "\n"
+    #    if hasattr(e, 'stdout'):
+    #        result += result + 'stderr: ' + e.stdout.decode('utf8') + "\n"
+
+    return HttpResponse(text)
