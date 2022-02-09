d00c63c86425a9a53deeb69e47d9280bcbab4a46
Nay Htoo Aung <72061499+shamzam@users.noreply.github.com>
2022-02-10 08:03:48 +0900

    Update views.py

--- login/views.py
+++ login/views.py
@@ -4,7 +4,7 @@ import subprocess
 
 def login(request):
     context = {}
-    if 'loggedIn' not in request.session:
+    if 'loggedIn' not in request.session or request.session['loggedIn'] == False:
         return render(request, 'login/login.html', context)
     else:
         return render(request, 'login/test.html', context)
