00b7fa2f7feb93984e75ae2512e09e875bce0015
Nay Htoo Aung <72061499+shamzam@users.noreply.github.com>
2022-02-09 23:03:30 +0900

    Update urls.py

--- mymanager/urls.py
+++ mymanager/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    path('/', include('login.urls')),
     path('pull/', include('pull.urls')),
-    path(r'/', include('login.urls')),
 ]
